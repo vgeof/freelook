bf89a81deabc2d77086ba18a9cd554afd4f96ba5
vgeof <Vous@exemple.com>
2020-01-14 23:19:11 +0100

    event based browser interaction

--- src/browser/main.js
+++ src/browser/main.js
@@ -1,25 +1,34 @@
-(function () {
+(function() {
+  const { ipcRenderer, remote } = require("electron");
+  document.addEventListener("emailPrompt", fillEmail);
+  window.setInterval(function() {
+    console.log("heartbeat...");
+    createEvents();
+  }, 1000);
 
-	window.addEventListener('onreadystatechange',() => {
-
-        if(isLoginPage())
-        {
-		    console.log("coucou");
-        }
-        if(document.readyState === 'complete') {
-        findEmailInput().value = 'toto'
-        }
-
-        
-    function isLoginPage()
-        {
-        return document.URL.includes("login");
-        }
-        function findEmailInput()
-        {
-            console.log(document.querySelector('body'));
-            return document.querySelector('input[type=email]')
-        }
-
-	});}())
-
+  function fillEmail() {
+    const email = "test@outlook.fr";
+    findEmailInput().value = email;
+    if (findEmailInput().value === email) {
+      console.log(findNextButton());
+      findNextButton().focus();
+      findNextButton().click();
+    }
+  }
+  function createEvents() {
+    // dispatch an event when the login screen asking for email appears
+    if (isLoginPage() && findEmailInput()) {
+      var emailPrompt = new Event("emailPrompt");
+      document.dispatchEvent(emailPrompt);
+    }
+  }
+  function isLoginPage() {
+    return document.URL.includes("login");
+  }
+  function findEmailInput() {
+    return document.querySelector("input[type=email]");
+  }
+  function findNextButton() {
+    return document.querySelector("input[type=submit]");
+  }
+})();
