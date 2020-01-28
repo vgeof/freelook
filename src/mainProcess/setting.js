047215399ffa4be73742598ba2c3b352441cd397
vgeoffroy <vgeoffroy@ncelrnd2442.nce.amadeus.net>
2020-01-28 16:12:47 +0100

    refactoring settings

--- src/mainProcess/setting.js
+++ src/mainProcess/setting.js
@@ -1,47 +1,65 @@
 const settings = require("electron-settings");
 
-$(() => {
-  loadSettings();
-});
+saveSettings = () => {
+  const verticalInput = document.querySelector(
+    "#ads-blocker-vertical-class input"
+  );
+  settings.set("verticalAdsClass", verticalInput.value);
+  const smallInput = document.querySelector("#ads-blocker-small-class input");
+  settings.set("smallAdsClass", smallInput.value);
 
+  const premiumInput = document.querySelector(
+    "#ads-blocker-premium-class input"
+  );
+  settings.set("premiumAdsClass", premiumInput.value);
+  // load home url setting
+  const homepageUrlDom = document.querySelector("#homepageUrl");
+  settings.set("homepageUrl", homepageUrlDom.value);
+
+  const defaultEmailInput = document.querySelector("#email-value input");
+  settings.set("default-email", defaultEmailInput.value);
+};
 loadSettings = () => {
   // load ads blocker setting
   const verticalClass = settings.get("verticalAdsClass");
-  const $verticalInput = $("#ads-blocker-vertical-class input");
-  $verticalInput.val(verticalClass);
-  $verticalInput.change(() =>
-    settings.set("verticalAdsClass", $verticalInput.val())
+  const verticalInput = document.querySelector(
+    "#ads-blocker-vertical-class input"
   );
+  if (verticalClass) verticalInput.value = verticalClass;
 
   const smallClass = settings.get("smallAdsClass");
-  const $smallInput = $("#ads-blocker-small-class input");
-  $smallInput.val(smallClass);
-  $smallInput.change(() => settings.set("smallAdsClass", $smallInput.val()));
+  const smallInput = document.querySelector("#ads-blocker-small-class input");
+  if (smallClass) smallInput.value = smallClass;
 
   const premiumClass = settings.get("premiumAdsClass");
-  const $premiumInput = $("#ads-blocker-premium-class input");
-  $premiumInput.val(premiumClass);
-  $premiumInput.change(() =>
-    settings.set("premiumAdsClass", $premiumInput.val())
+  const premiumInput = document.querySelector(
+    "#ads-blocker-premium-class input"
   );
+  if (premiumClass) premiumInput.value = premiumClass;
 
   // load home url setting
   const homepageUrl = settings.get(
     "homepageUrl",
     "https://outlook.live.com/mail"
   );
-  let $homepageUrl = $("#homepageUrl");
-  $homepageUrl.dropdown("set selected", homepageUrl);
+  const homepageUrlDom = document.querySelector("#homepageUrl");
+  homepageUrlDom.value = homepageUrl;
 
-  $homepageUrl.dropdown({
-    onChange: value => {
-      settings.set("homepageUrl", value);
-    }
-  });
   const defaultEmail = settings.get("default-email");
-  const $defaultEmailInput = $("#email-value input");
-  $defaultEmailInput.val(defaultEmail);
-  $defaultEmailInput.change(() =>
-    settings.set("default-email", $defaultEmailInput.val())
-  );
+  console.log(defaultEmail);
+  const defaultEmailInput = document.querySelector("#email-value input");
+  if (defaultEmail) defaultEmailInput.value = defaultEmail;
+  else defaultEmailInput.value = "";
 };
+
+loadSettings();
+var saveButton = document.querySelector("#saveButton");
+saveButton.addEventListener("click", saveSettings);
+var cancelButton = document.querySelector("#cancelButton");
+cancelButton.addEventListener("click", loadSettings);
+var resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener("click", () => {
+  console.log("resetting settings");
+  settings.deleteAll();
+  loadSettings();
+});
